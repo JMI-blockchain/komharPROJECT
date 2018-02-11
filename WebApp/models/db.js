67088be70ad6470a0e0bb1cc556692f656f77f84
heman96 <himanshubackupbox@gmail.com>
2018-02-11 18:32:16 +0530

    Update db.js

--- WebApp/models/db.js
+++ WebApp/models/db.js
@@ -6,14 +6,18 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 mongoose.connect('mongodb://admin:admin@ds219318.mlab.com:19318/notarization-db');
 
-
 var Schema = mongoose.Schema;
 
 // create a schema
 var userSchema = new Schema({
   name: String,
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }
+  //encPrivateKey: {type: String,required: false}
+});
+var keySchema = new Schema({
+	username: {type: String, required: true, unique: true},
+	encKey: {type: String}
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -21,5 +25,9 @@ userSchema.plugin(passportLocalMongoose);
 // we need to create a model using it
 var User = mongoose.model('User', userSchema);
 
+var Key = mongoose.model('key',keySchema);
 // make this available to our users in our Node applications
-module.exports = User;
+module.exports = {
+	'User': User,
+	'Key': Key
+};
