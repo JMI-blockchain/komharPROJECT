31c5923dc656413aa3b15c32949854ef55d9a4f6
heman96 <himanshubackupbox@gmail.com>
2018-02-11 18:33:53 +0530

    Update web3.js

--- WebApp/models/web3.js
+++ WebApp/models/web3.js
@@ -1,30 +1,37 @@
-//wallet creation and encrypting wallet using password
-//still in progress
+//wallet creation when a new user registers
+//not fully functional yet!
 
 var Web3 = require('web3');
 
 var web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/d0FjnUKmW0TQrK9cci9m"));
 
-var wallet = web3.eth.accounts.wallet.create(1, '54674321§3456764321§345674321§3453647544±±±§±±±!!!43534534534534');
 //console.log(wallet);
 
 //console.log('*********************************************************');
 
-var arr = web3.eth.accounts.wallet.encrypt('password');
-
+//console.log(arr);
 //keyfile of the generated wallet
-var publicKey = '0x'+arr[0].address;
-var encPrivateKey = arr[0].crypto.ciphertext;
+//var publicKey = '0x'+arr[0].address;
+//var encPrivateKey = arr[0].crypto.ciphertext;
 
-var keyFile = {'publicKey':publicKey,'encPrivateKey':encPrivateKey};
+//var keyFile = {'publicKey':publicKey,'encPrivateKey':encPrivateKey};
 
 //console.log('0x'+arr[0].address);
 //console.log(keyFile);
 
-module.exports = publicKey;
-module.exports = encPrivateKey;
+//module.exports = publicKey;
+module.exports = function(password){
+	var wallet = web3.eth.accounts.wallet.create(1, '54674321§3456764321§345674321§3453647544±±±§±±±!!!43534534534534');
+	var arr = web3.eth.accounts.wallet.encrypt(password);
+	var key = arr[0].crypto.ciphertext;
+	return key;
+}
+
 //console.log('*********************************************************');
 
 //console.log(web3.version);
+
+
+
 
 
