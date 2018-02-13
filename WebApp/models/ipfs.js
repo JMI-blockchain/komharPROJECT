605ec329a04a70bbfd3b75edc56009c71906577e
amish gupta <amishgupta66@gmail.com>
2018-02-13 09:29:13 +0530

    ipfs module added: not tested

---
+++ WebApp/models/ipfs.js
@@ -0,0 +1,37 @@
+var ipfsAPI = require('ipfs-api')
+var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'}) 
+const IPFS = require('ipfs');
+// Spawn your IPFS node \o/
+const node = new IPFS();
+var fs = require('fs');
+
+function ipfsupload(filepath){
+	
+	node.on('ready', () => {
+    node.id((err, id) => {
+        if (err) {
+            return console.log(err)
+        }
+        console.log(id)
+    })
+    
+var data = fs.readFileSync(filepath);
+console.log("Synchronous read: " + data.toString());
+
+    let files = [
+        {
+            path: path,
+            content: data
+        }
+    ]
+    node.files.add(files, function (err, files) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(files)
+        }
+    })
+})
+}
+
+module.exports.ipfsupload=ipfsupload;
